1e6a3d91ae0dea3d467807d982c81beddc7e8f6d
Ivo Georgiev <ivogeorg@gmail.com>
2019-10-14 16:48:55 -0600

    Added a dummy .java file to discuss java vs js

---
+++ file.java
@@ -0,0 +1,14 @@
+// The differences between Java (*.java) and JavaScript (*.js) are extensive
+// Here is one of the better articles on the subject (Note: Advanced reading!): 
+// https://hackernoon.com/java-vs-javascript-heres-what-you-need-to-know-9b1b708394e2
+
+// Instead of looking for the differences, just ignore the poor naming strategy
+// and treat them as two random languages that have little in common to warrant
+// such misleadingly similar names.
+
+// Note that while vanilla JavaScript does not support classes, instead having
+// prototype-based inheritance, Static TypeScript (STS), used in Makecode for the 
+// micro:bit, does support classes, which syntactically are closer to Java than
+// JavaScript. How they are supported internally is another matter altogether.
+// The gory details can be found in this paper: 
+// https://www.microsoft.com/en-us/research/publication/static-typescript/
